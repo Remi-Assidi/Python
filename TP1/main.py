bd99eb039496e33a8865a1235dfc70a277748b81
Remi-Assidi <remi.assidi@epsi.fr>
2020-11-23 10:14:00 +0100

    First Programme

---
+++ TP1/main.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/python3
+prix = 0.0
+quantite = 0.0
+
+while True:
+    prix = float(input("Veuillez saisir le prix (HT) de votre produit: "))
+    if prix <= 0:
+        print("Erreur votre prix doit être supérieur à 0.")
+    else:
+        break
+
+while True:
+    quantite = float(input("Veuillez saisir la quantité: "))
+    if quantite <= 0:
+        print("Erreur votre quantite doit être supérieur à 0.")
+    else:
+        break
+
+prixTotal = prix*quantite*1.2
+
+if prixTotal > 200:
+    prixTotal = prixTotal*0.95
+    print("Vous avez eu une remise de 5% car votre commande est supérieur à 200 euros.")
+print("Le prix total est de {:.2f}".format(prixTotal))
