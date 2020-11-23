9f1ff1da4043cde423571cf5948d16798ef32c20
Remi-Assidi <remi.assidi@epsi.fr>
2020-11-23 11:57:56 +0100

    Add functions

--- TP2/main.py
+++ TP2/main.py
@@ -3,7 +3,29 @@
 
 listeDesProduits = {1:{"Nom":"Banane","Prix":4.0,"Quantite":0}, 2:{"Nom":"Pomme","Prix":2.0,"Quantite":0}, 3:{"Nom":"Orange","Prix":1.5,"Quantite":0}, 4:{"Nom":"Poire","Prix":3.0,"Quantite":0}}
 
-def display(listeDesProduits):
+def testerSiIdentifiantExist(identifiant):
+    if identifiant in listeDesProduits.keys():
+        return True
+    else: 
+        return False
+
+def demanderQuantite():
+    while True:
+        quantite = float(input("Veuillez saisir la quantité: "))
+        if quantite <= 0:
+            print("Erreur votre quantite doit être supérieur à 0.")
+        else:
+            listeDesProduits[identifiant]["Quantite"] = quantite
+            break
+
+def testerSiNePLusSaisirDeProduit():
+    saisirAutreProduit = input("Voulez vous saisir un autre produit ? (y/n) ")
+    if saisirAutreProduit != "y":
+        return False
+    else:
+        return True
+
+def afficherTotal(listeDesProduits):
     prixTotal = 0.0
     print("+----------------------------------+---------+------------------------+----------------+")
     print("+------------NOM-------------------+--Prix---+--------Quantité--------+-----Total-HT---+")
@@ -20,26 +42,13 @@ def display(listeDesProduits):
     print("Total HT: {:.2f}".format(prixTotal))
     print("Le prix total TTC est de {:.2f}".format(prixTotal*1.20))
 
-def demanderQuantite():
-    while True:
-        quantite = float(input("Veuillez saisir la quantité: "))
-        if quantite <= 0:
-            print("Erreur votre quantite doit être supérieur à 0.")
-        else:
-            listeDesProduits[identifiant]["Quantite"] = quantite
-            break
-
 while True:
     identifiant = int(input("Veuillez saisir l'identifiant de votre produit: "))
-    try:
-        if identifiant in listeDesProduits.keys():
-            demanderQuantite()
-        else: 
-            raise Exception("L'identifiant n'existe pas.")
-    except Exception as e:
-        print(e)
+    if testerSiIdentifiantExist(identifiant):
+        demanderQuantite()
+    else:
         print("Erreur, l'identifiant n'existe pas, veuillez reessayer.")
-    saisirAutreProduit = input("Voulez vous saisir un autre produit ? (y/n) ")
-    if saisirAutreProduit != "y":
+    if not testerSiNePLusSaisirDeProduit():
         break
-display(listeDesProduits)
+
+afficherTotal(listeDesProduits)
