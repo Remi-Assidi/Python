d5826adb3553f2a6d2929ce0b9cc557628b14fcc
Remi-Assidi <remi.assidi@epsi.fr>
2020-11-24 09:57:46 +0100

    Moved functions

--- TP2/main.py
+++ TP2/main.py
@@ -1,54 +1,29 @@
 # -*- coding: utf-8 -*-
 #!/usr/bin/python3
+from utils import *;
 
-listeDesProduits = {1:{"Nom":"Banane","Prix":4.0,"Quantite":0}, 2:{"Nom":"Pomme","Prix":2.0,"Quantite":0}, 3:{"Nom":"Orange","Prix":1.5,"Quantite":0}, 4:{"Nom":"Poire","Prix":3.0,"Quantite":0}}
+def main():
+    listeDesProduits = {1:{"Nom":"Banane","Prix":4.0,"Quantite":0}, 2:{"Nom":"Pomme","Prix":2.0,"Quantite":0}, 3:{"Nom":"Orange","Prix":1.5,"Quantite":0}, 4:{"Nom":"Poire","Prix":3.0,"Quantite":0}}
 
-def testerSiIdentifiantExist(identifiant):
-    if identifiant in listeDesProduits.keys():
-        return True
-    else: 
-        return False
-
-def demanderQuantite():
     while True:
-        quantite = float(input("Veuillez saisir la quantité: "))
-        if quantite <= 0:
-            print("Erreur votre quantite doit être supérieur à 0.")
+        identifiant = int(input("Veuillez saisir l'identifiant de votre produit: "))
+        if testerSiIdentifiantExist(identifiant, listeDesProduits):
+            listeDesProduits[identifiant]["Quantite"] = demanderQuantite()
         else:
-            listeDesProduits[identifiant]["Quantite"] = quantite
+            print("Erreur, l'identifiant n'existe pas, veuillez reessayer.")
+        if not testerSiNePLusSaisirDeProduit():
             break
 
-def testerSiNePLusSaisirDeProduit():
-    saisirAutreProduit = input("Voulez vous saisir un autre produit ? (y/n) ")
-    if saisirAutreProduit != "y":
-        return False
-    else:
-        return True
+    afficherTotal(listeDesProduits)
+    prixTotalHT = calculerTotal(listeDesProduits)
+    prixApresRemise = calculerRemise(0.95, 200.0, prixTotalHT)
+    remise = prixTotalHT - prixApresRemise
+    prixTotalTTC= calculerTotalTTC(prixApresRemise)
 
-def afficherTotal(listeDesProduits):
-    prixTotal = 0.0
-    print("+----------------------------------+---------+------------------------+----------------+")
-    print("+------------NOM-------------------+--Prix---+--------Quantité--------+-----Total-HT---+")
-    for produit in listeDesProduits:
-        if listeDesProduits[produit]["Quantite"] != 0:
-            totalht = listeDesProduits[produit]["Prix"]*listeDesProduits[produit]["Quantite"]
-            print("|           {Nom}                 |   {Prix}   |          {Quantite}          |     {TotalHT}     |".format(Nom=listeDesProduits[produit]["Nom"], Prix=listeDesProduits[produit]["Prix"], Quantite=listeDesProduits[produit]["Quantite"], TotalHT=totalht))
-            prixTotal += totalht
-    print("+----------------------------------+---------+------------------------+----------------+")
-    if prixTotal > 200:
-        print("Sous total HT: {:.2f}".format(prixTotal))
-        print("Remise 5%: {:.2f}".format(prixTotal-(prixTotal*0.95)))
-        prixTotal = prixTotal*0.95
-    print("Total HT: {:.2f}".format(prixTotal))
-    print("Le prix total TTC est de {:.2f}".format(prixTotal*1.20))
+    print("Sous total HT: {:.2f}".format(prixTotalHT))
+    print("Remise 5%: {:.2f}".format(remise))
+    print("Total HT: {:.2f}".format(prixApresRemise))
+    print("Le prix total TTC est de {:.2f}".format(prixTotalTTC))
 
-while True:
-    identifiant = int(input("Veuillez saisir l'identifiant de votre produit: "))
-    if testerSiIdentifiantExist(identifiant):
-        demanderQuantite()
-    else:
-        print("Erreur, l'identifiant n'existe pas, veuillez reessayer.")
-    if not testerSiNePLusSaisirDeProduit():
-        break
-
-afficherTotal(listeDesProduits)
+if __name__ == "__main__":
+    main()
---
+++ TP2/utils.py
@@ -0,0 +1,44 @@
+def testerSiIdentifiantExist(identifiant, listeDesProduits):
+    if identifiant in listeDesProduits.keys():
+        return True
+    else: 
+        return False
+
+def testerSiNePLusSaisirDeProduit():
+    saisirAutreProduit = input("Voulez vous saisir un autre produit ? (y/n) ")
+    if saisirAutreProduit != "y":
+        return False
+    else:
+        return True
+
+def demanderQuantite():
+    while True:
+        quantite = float(input("Veuillez saisir la quantité: "))
+        if quantite <= 0:
+            print("Erreur votre quantite doit être supérieur à 0.")
+        else:
+            return quantite
+
+def afficherTotal(listeDesProduits):
+    print("+----------------------------------+---------+------------------------+----------------+")
+    print("+------------NOM-------------------+--Prix---+--------Quantité--------+-----Total-HT---+")
+    for produit in listeDesProduits:
+        if listeDesProduits[produit]["Quantite"] != 0:
+            totalht = listeDesProduits[produit]["Prix"]*listeDesProduits[produit]["Quantite"]
+            print("|           {Nom}                 |   {Prix}   |          {Quantite}          |     {TotalHT}     |".format(Nom=listeDesProduits[produit]["Nom"], Prix=listeDesProduits[produit]["Prix"], Quantite=listeDesProduits[produit]["Quantite"], TotalHT=totalht))
+    print("+----------------------------------+---------+------------------------+----------------+")
+
+def calculerTotal(listeDesProduits):
+    prixTotal = 0.0
+    for produit in listeDesProduits:
+        if listeDesProduits[produit]["Quantite"] != 0:
+            totalht = listeDesProduits[produit]["Prix"]*listeDesProduits[produit]["Quantite"]
+            prixTotal += totalht
+    return prixTotal
+
+def calculerTotalTTC(prixTotalHT):
+    return prixTotalHT * 1.20
+
+def calculerRemise(pourcentageRemise, limiteDeclanchement, prixTotalHT):
+    if prixTotalHT > limiteDeclanchement:
+        return prixTotalHT*pourcentageRemise
