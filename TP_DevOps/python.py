3348e6babe8743607396b2e2e52b4fd7f73badc8
Remi-Assidi <remi.assidi@epsi.fr>
2021-01-11 11:56:54 +0100

    Modified thirst part of the script

--- TP_DevOps/python.py
+++ TP_DevOps/python.py
@@ -1,11 +1,32 @@
 import argparse
-import csv
+import os
+import glob
+import pandas as pd
+
+def createFolders(path, folder):
+    for index, row in df.iterrows():
+        try:
+            pathWithDepartment = path + row["nom_region"] + "/" + row["nom_departement"]
+            if os.path.exists(pathWithDepartment):
+                if len(os.listdir(pathWithDepartment)) > 0:
+                    if input("Le dossier " + pathWithDepartment + " contient des fichiers, voulez vous les supprimer ? ") != "yes":
+                        continue
+                    else:
+                        files = glob.glob(pathWithDepartment + "/*")
+                        for f in files:
+                            os.remove(f)
+                        os.rmdir(pathWithDepartment)
+                else:
+                    continue
+            os.makedirs(pathWithDepartment, exist_ok=True)
+        except Exception as e:
+            print(e)
+        else:
+            print ("Successfully created the directory %s " % pathWithDepartment)
 
 parser = argparse.ArgumentParser()
 parser.add_argument("file", help="Name of the csv file containing CSV datas")
 args = parser.parse_args()
 print(args.file)
-with open(args.file, newline='') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=' ', quotechar=',')
-    for row in spamreader:
-        print(', '.join(row))
+df = pd.read_csv(args.file, usecols=['code_departement', 'nom_departement', 'code_region', 'nom_region'])
+createFolders("./departements/", df)
